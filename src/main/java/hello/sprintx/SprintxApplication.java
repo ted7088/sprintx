43a5ed0b1b40b60415ac741aea0eb7feab36a817
master <ted3131@diquest.com>
2025-04-07 14:45:02 +0900

    Initial commit

---
+++ src/main/java/hello/sprintx/SprintxApplication.java
@@ -0,0 +1,13 @@
+package hello.sprintx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintxApplication.class, args);
+	}
+
+}
